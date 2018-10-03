249c5a15741bd561b17a122bbac1fb40c0736af8
maruyama kyousuke <maruyama@alitz.co.jp>
2018-10-03 20:04:44 +0900

    tableの作成

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,16 @@ v-app
       v-slide-y-transition(mode='out-in')
         v-layout(column='', align-center='')
           img.mb-5(src='@/assets/logo.png', alt='Vuetify.js')
-          blockquote
-            | “First, solve the problem. Then, write the code.”
-            footer
-              small
-                em —John Johnson
+          v-data-table(:headers='headers', :items='key_bind' hide-actions expand)
+            template(slot='items', slot-scope="props")
+              td.key-td
+                | {{props.item.key}}
+              td.command-td
+                | {{props.item.command}}
+              td.when-td
+                | {{props.item.when}}
+              td.comment-td
+                | {{props.item.comment}}
   v-footer(:fixed='fixed', app='')
     span © 2017
 
@@ -24,13 +29,36 @@ export default {
   },
   data () {
     return {
+      headers: [
+        { text: 'key', value: 'key' },
+        { text: 'command', value: 'command' },
+        { text: 'when', value: 'when' },
+        { text: 'コメント', value: 'comment' },
+      ],
       key_bind: keyBind,
       fixed: false,
       title: 'Vuetify.js'
     }
   },
   created () {
-    console.log(this.key_data[0])
+    console.log(this.key_bind)
   }
 }
 </script>
+<style lang='sass' scoped>
+td.key-td
+  width: 200px
+  padding-right: 20px
+  overflow-wrap: break-word
+td.command-td
+  max-width: 200px
+  padding-right: 20px
+  overflow-wrap: break-word
+td.when-td
+  max-width: 200px
+  padding-right: 20px
+  overflow-wrap: break-word
+td.comment-td
+  max-width: 300px
+  overflow-wrap: break-word
+</style>
