88d18f9cd82064e7bc4e926fed59744a593305dd
maruyama kyousuke <maruyama@alitz.co.jp>
2018-10-04 12:05:15 +0900

    commandとwhenでfilterをかけられるようにする

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template lang='pug'>
 v-app
   v-content
-    v-container(fluid='')
-      v-slide-y-transition(mode='out-in')
-        v-layout(column='', align-center='')
+    v-container(grid-list-md text-xs-center)
+      v-layout(row wrap)
+        v-flex(xs12)
           img.mb-5(src='@/assets/logo.png', alt='Vuetify.js')
-          v-data-table(:headers='headers', :items='key_bind' hide-actions expand)
-            template(slot='items', slot-scope="props")
-              td.key-td
-                | {{props.item.key}}
-              td.command-td
-                | {{props.item.command}}
-              td.when-td
-                | {{props.item.when}}
-              td.comment-td
-                | {{props.item.comment}}
+        v-flex(xs12)
+          v-card
+            v-card-text
+              v-form
+                v-text-field(v-model='command' label="command")
+                v-text-field(v-model='when' label="when")
+        v-flex(xs12)
+          v-card.text-xs-left
+            v-card-text
+              v-data-table(:headers='headers', :items='keyBind' hide-actions expand)
+                template(slot='items', slot-scope="props")
+                  td.key-td
+                    | {{props.item.key}}
+                  td.command-td
+                    | {{props.item.command}}
+                  td.when-td
+                    | {{props.item.when}}
+                  td.comment-td
+                    | {{props.item.comment}}
   v-footer(:fixed='fixed', app='')
     span © 2017
 
 </template>
 
 <script>
-import keyBind from './default_keys.json'
+import defaultKeyBind from './default_keys.json'
 
 export default {
   name: 'App',
@@ -30,19 +39,43 @@ export default {
   data () {
     return {
       headers: [
-        { text: 'key', value: 'key' },
-        { text: 'command', value: 'command' },
-        { text: 'when', value: 'when' },
-        { text: 'コメント', value: 'comment' },
+        { text: 'key', value: 'key', sortable: false },
+        { text: 'command', value: 'command', sortable: false },
+        { text: 'when', value: 'when', sortable: false },
+        { text: 'コメント', value: 'comment', sortable: false }
       ],
-      key_bind: keyBind,
       fixed: false,
-      title: 'Vuetify.js'
+      title: 'Vuetify.js',
+      command: '',
+      when: ''
     }
   },
-  created () {
-    console.log(this.key_bind)
-  }
+  computed: {
+    keyBind () {
+      let keyBind = Object.assign([], defaultKeyBind)
+
+      if (this.command) {
+        keyBind = keyBind.filter(value => {
+          if (value.command) {
+            return value.command.indexOf(this.command) !== -1
+          } else {
+            return false
+          }
+        })
+      }
+
+      if (this.when) {
+        keyBind = keyBind.filter(value => {
+          if (value.when) {
+            return value.when.indexOf(this.when) !== -1
+          } else {
+            return false
+          }
+        })
+      }
+      return keyBind
+    }
+  },
 }
 </script>
 <style lang='sass' scoped>
