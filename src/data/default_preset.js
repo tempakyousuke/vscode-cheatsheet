16df09e0620b9d76cd3f46efc363b8dfeb82e13a
kyousuke maruyama <tempakyousuke@gmail.com>
2018-12-04 21:04:13 +0900

    プリセット更新

--- src/data/default_preset.js
+++ src/data/default_preset.js
@@ -15,13 +15,13 @@ export const defaultPreset = [
     name: '移動',
     command: 'hogemoge',
     when: '',
-    contains: 'cursorTop\ncursorBottom\ncursorHome\ncursorLineStart\ncursorLineEnd\ncursorEnd\ncursorHome\neditor.action.toggleWordWrap\ncursorWordStartLeft\ncursorWordEndRight\nscrollLineUp\nscrollLineDown\nscrollPageUp\nscrollPageDown\neditor.action.jumpToBracket\nworkbench.action.navigateForward\nworkbench.action.navigateBack\nworkbench.action.navigateToLastEditLocation\ncursorWordEndLeft\ncursorWordLeft\ncursorWordRight\ncursorWordPartLeft\ncursorWordPartRight\ncursorWordPartStartLeft\n'
+    contains: 'cursorTop\ncursorBottom\ncursorHome\ncursorLineStart\ncursorLineEnd\ncursorEnd\ncursorHome\neditor.action.toggleWordWrap\ncursorWordStartLeft\ncursorWordEndRight\ncursorPageDown\ncursorPageUp\nscrollLineUp\nscrollLineDown\nscrollPageUp\nscrollPageDown\neditor.action.jumpToBracket\nworkbench.action.navigateForward\nworkbench.action.navigateBack\nworkbench.action.navigateToLastEditLocation\ncursorWordEndLeft\ncursorWordLeft\ncursorWordRight\ncursorWordPartLeft\ncursorWordPartRight\ncursorWordPartStartLeft\n'
   },
   {
     name: '選択',
     command: 'hogemoge',
     when: '',
-    contains: 'cursorTopSelect\ncursorBottomSelect\ncursorHomeSelect\ncursorWordEndLeftSelect\ncursorWordLeftSelect\ncursorWordRightSelect\ncursorWordPartLeftSelect\ncursorWordPartRightSelect\ncursorWordPartStartLeftSelect\neditor.action.smartSelect.grow\neditor.action.smartSelect.shrink'
+    contains: 'cursorTopSelect\ncursorBottomSelect\ncursorHomeSelect\ncursorPageDownSelect\ncursorPageUpSelect\ncursorWordEndLeftSelect\ncursorWordLeftSelect\ncursorWordRightSelect\ncursorWordPartLeftSelect\ncursorWordPartRightSelect\ncursorWordPartStartLeftSelect\neditor.action.smartSelect.grow\neditor.action.smartSelect.shrink'
   },
   {
     name: '削除',
